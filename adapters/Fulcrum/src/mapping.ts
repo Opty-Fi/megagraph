13cd8b3ebef269caf9a4461beb8958123fb8da6c
undermethod <shaunlerner@gmail.com>
2021-06-23 18:15:29 -0600

    updating Fulcrum adapter, 'bZxProtocol', 'StakingProxy' and 'tokens' folder

--- adapters/Fulcrum/src/mapping.ts
+++ adapters/Fulcrum/src/mapping.ts
@@ -1,10 +1,10 @@
-import { OwnershipTransferred as OwnershipTransferredEvent } from "./bZxProtocol"
-import { OwnershipTransferred } from "./schema"
+import { OwnershipTransferred as OwnershipTransferredEvent } from "../generated/bZxProtocol/bZxProtocol"
+import { bZxProtocolOwnershipTransferred } from "../generated/schema"
 
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
 ): void {
-  let entity = new OwnershipTransferred(
+  let entity = new bZxProtocolOwnershipTransferred(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   )
   entity.previousOwner = event.params.previousOwner
