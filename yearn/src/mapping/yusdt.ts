cb75ec71f81ca8f27a43ac5378664fe11216c17d
leodinh <leo.atdinh@gmail.com>
2021-06-28 20:49:03 -0400

    added more vaults

---
+++ yearn/src/mapping/yusdt.ts
@@ -0,0 +1,56 @@
+import {
+  yearn_yUSDT,
+  DepositCall,
+  WithdrawCall
+} from "../../generated/yearn_yUSDT/yearn_yUSDT"
+import { Balance, PricePerFullShare } from "../../generated/schema"
+
+export function handleDeposit(call: DepositCall): void {
+  let contract = yearn_yUSDT.bind(call.to)
+  let pricePerFullShare = contract.try_getPricePerFullShare()
+  if (!pricePerFullShare.reverted) {
+    let entity = new PricePerFullShare(call.transaction.hash.toHexString())
+    entity.pricePerFullShare = pricePerFullShare.value
+    entity.blockNumber = call.block.number
+    entity.timestamp = call.block.timestamp
+    entity.token = contract.symbol()
+    entity.vault = call.to.toHexString()
+    entity.save()
+  }
+
+  let balance = contract.try_balance()
+  if (!balance.reverted) {
+    let entity = new Balance(call.transaction.hash.toHexString())
+    entity.balance = balance.value
+    entity.blockNumber = call.block.number
+    entity.timestamp = call.block.timestamp
+    entity.token = contract.symbol()
+    entity.vault = call.to.toHexString()
+    entity.save()
+  }
+}
+
+export function handleWithdraw(call: WithdrawCall): void {
+  let contract = yearn_yUSDT.bind(call.to)
+  let pricePerFullShare = contract.try_getPricePerFullShare()
+  if (!pricePerFullShare.reverted) {
+    let entity = new PricePerFullShare(call.transaction.hash.toHexString())
+    entity.pricePerFullShare = pricePerFullShare.value
+    entity.blockNumber = call.block.number
+    entity.timestamp = call.block.timestamp
+    entity.token = contract.symbol()
+    entity.vault = call.to.toHexString()
+    entity.save()
+  }
+
+  let balance = contract.try_balance()
+  if (!balance.reverted) {
+    let entity = new Balance(call.transaction.hash.toHexString())
+    entity.balance = balance.value
+    entity.blockNumber = call.block.number
+    entity.timestamp = call.block.timestamp
+    entity.token = contract.symbol()
+    entity.vault = call.to.toHexString()
+    entity.save()
+  }
+}
