dd1e454484d5e01551c912613cbe7d2c3d0e6ecc
undermethod <shaunlerner@gmail.com>
2021-07-05 10:24:27 -0600

    WIP update to refactor 'FromTokenToPool' and 'AaveV1Token'

--- adapters/AaveV1/src/mapping.AToken.ts
+++ adapters/AaveV1/src/mapping.AToken.ts
@@ -1,3 +1,4 @@
+import { log, Address, Bytes, BigInt } from "@graphprotocol/graph-ts";
 import {
   Approval as ApprovalEvent,
   BalanceTransfer as BalanceTransferEvent,
@@ -8,121 +9,113 @@ import {
   Redeem as RedeemEvent,
   RedirectedBalanceUpdated as RedirectedBalanceUpdatedEvent,
   Transfer as TransferEvent
-} from "./AToken"
-import {
-  Approval,
-  BalanceTransfer,
-  BurnOnLiquidation,
-  InterestRedirectionAllowanceChanged,
-  InterestStreamRedirected,
-  MintOnDeposit,
-  Redeem,
-  RedirectedBalanceUpdated,
-  Transfer
-} from "../generated/schema"
+} from "./AToken";
+import { FromTokenToPool, AaveV1Token } from "../generated/schema";
+// import { AaveTokenV1 as UnderlyingAsset } from "./AaveTokenV1";
+import { convertBINumToDesiredDecimals } from "./converters";
+
+function handleToken(
+  transactionHash: Bytes,
+  blockNumber: BigInt,
+  blockTimestamp: BigInt,
+  address: Address,
+  // todo
+): void {
+  // todo
+}
 
 export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleBalanceTransfer(event: BalanceTransferEvent): void {
-  let entity = new BalanceTransfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._from = event.params._from
-  entity._to = event.params._to
-  entity._value = event.params._value
-  entity._fromBalanceIncrease = event.params._fromBalanceIncrease
-  entity._toBalanceIncrease = event.params._toBalanceIncrease
-  entity._fromIndex = event.params._fromIndex
-  entity._toIndex = event.params._toIndex
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleBurnOnLiquidation(event: BurnOnLiquidationEvent): void {
-  let entity = new BurnOnLiquidation(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._from = event.params._from
-  entity._value = event.params._value
-  entity._fromBalanceIncrease = event.params._fromBalanceIncrease
-  entity._fromIndex = event.params._fromIndex
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleInterestRedirectionAllowanceChanged(
   event: InterestRedirectionAllowanceChangedEvent
 ): void {
-  let entity = new InterestRedirectionAllowanceChanged(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._from = event.params._from
-  entity._to = event.params._to
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleInterestStreamRedirected(
   event: InterestStreamRedirectedEvent
 ): void {
-  let entity = new InterestStreamRedirected(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._from = event.params._from
-  entity._to = event.params._to
-  entity._redirectedBalance = event.params._redirectedBalance
-  entity._fromBalanceIncrease = event.params._fromBalanceIncrease
-  entity._fromIndex = event.params._fromIndex
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleMintOnDeposit(event: MintOnDepositEvent): void {
-  let entity = new MintOnDeposit(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._from = event.params._from
-  entity._value = event.params._value
-  entity._fromBalanceIncrease = event.params._fromBalanceIncrease
-  entity._fromIndex = event.params._fromIndex
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleRedeem(event: RedeemEvent): void {
-  let entity = new Redeem(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._from = event.params._from
-  entity._value = event.params._value
-  entity._fromBalanceIncrease = event.params._fromBalanceIncrease
-  entity._fromIndex = event.params._fromIndex
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleRedirectedBalanceUpdated(
   event: RedirectedBalanceUpdatedEvent
 ): void {
-  let entity = new RedirectedBalanceUpdated(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._targetAddress = event.params._targetAddress
-  entity._targetBalanceIncrease = event.params._targetBalanceIncrease
-  entity._targetIndex = event.params._targetIndex
-  entity._redirectedBalanceAdded = event.params._redirectedBalanceAdded
-  entity._redirectedBalanceRemoved = event.params._redirectedBalanceRemoved
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
--- adapters/AaveV1/src/mapping.LendingPool.ts
+++ adapters/AaveV1/src/mapping.LendingPool.ts
@@ -1,3 +1,4 @@
+import { log, Address, Bytes, BigInt } from "@graphprotocol/graph-ts";
 import {
   Borrow as BorrowEvent,
   Deposit as DepositEvent,
@@ -10,163 +11,135 @@ import {
   ReserveUsedAsCollateralDisabled as ReserveUsedAsCollateralDisabledEvent,
   ReserveUsedAsCollateralEnabled as ReserveUsedAsCollateralEnabledEvent,
   Swap as SwapEvent
-} from "../generated/LendingPool/LendingPool"
-import {
-  LendingPoolBorrow,
-  LendingPoolDeposit,
-  LendingPoolFlashLoan,
-  LendingPoolLiquidationCall,
-  LendingPoolOriginationFeeLiquidated,
-  LendingPoolRebalanceStableBorrowRate,
-  LendingPoolRedeemUnderlying,
-  LendingPoolRepay,
-  LendingPoolReserveUsedAsCollateralDisabled,
-  LendingPoolReserveUsedAsCollateralEnabled,
-  LendingPoolSwap
-} from "../generated/schema"
+} from "../generated/LendingPool/LendingPool";
+import { FromTokenToPool, AaveV1Token } from "../generated/schema";
+// import { AaveTokenV1 as UnderlyingAsset } from "./AaveTokenV1";
+import { convertBINumToDesiredDecimals } from "./converters";
+
+function handleToken(
+  transactionHash: Bytes,
+  blockNumber: BigInt,
+  blockTimestamp: BigInt,
+  address: Address,
+  // todo
+): void {
+  // todo
+}
 
 export function handleBorrow(event: BorrowEvent): void {
-  let entity = new LendingPoolBorrow(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity._amount = event.params._amount
-  entity._borrowRateMode = event.params._borrowRateMode
-  entity._borrowRate = event.params._borrowRate
-  entity._originationFee = event.params._originationFee
-  entity._borrowBalanceIncrease = event.params._borrowBalanceIncrease
-  entity._referral = event.params._referral
-  entity._timestamp = event.params._timestamp
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleDeposit(event: DepositEvent): void {
-  let entity = new LendingPoolDeposit(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity._amount = event.params._amount
-  entity._referral = event.params._referral
-  entity._timestamp = event.params._timestamp
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleFlashLoan(event: FlashLoanEvent): void {
-  let entity = new LendingPoolFlashLoan(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._target = event.params._target
-  entity._reserve = event.params._reserve
-  entity._amount = event.params._amount
-  entity._totalFee = event.params._totalFee
-  entity._protocolFee = event.params._protocolFee
-  entity._timestamp = event.params._timestamp
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleLiquidationCall(event: LiquidationCallEvent): void {
-  let entity = new LendingPoolLiquidationCall(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._collateral = event.params._collateral
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity._purchaseAmount = event.params._purchaseAmount
-  entity._liquidatedCollateralAmount = event.params._liquidatedCollateralAmount
-  entity._accruedBorrowInterest = event.params._accruedBorrowInterest
-  entity._liquidator = event.params._liquidator
-  entity._receiveAToken = event.params._receiveAToken
-  entity._timestamp = event.params._timestamp
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleOriginationFeeLiquidated(
   event: OriginationFeeLiquidatedEvent
 ): void {
-  let entity = new LendingPoolOriginationFeeLiquidated(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._collateral = event.params._collateral
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity._feeLiquidated = event.params._feeLiquidated
-  entity._liquidatedCollateralForFee = event.params._liquidatedCollateralForFee
-  entity._timestamp = event.params._timestamp
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleRebalanceStableBorrowRate(
   event: RebalanceStableBorrowRateEvent
 ): void {
-  let entity = new LendingPoolRebalanceStableBorrowRate(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity._newStableRate = event.params._newStableRate
-  entity._borrowBalanceIncrease = event.params._borrowBalanceIncrease
-  entity._timestamp = event.params._timestamp
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleRedeemUnderlying(event: RedeemUnderlyingEvent): void {
-  let entity = new LendingPoolRedeemUnderlying(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity._amount = event.params._amount
-  entity._timestamp = event.params._timestamp
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleRepay(event: RepayEvent): void {
-  let entity = new LendingPoolRepay(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity._repayer = event.params._repayer
-  entity._amountMinusFees = event.params._amountMinusFees
-  entity._fees = event.params._fees
-  entity._borrowBalanceIncrease = event.params._borrowBalanceIncrease
-  entity._timestamp = event.params._timestamp
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleReserveUsedAsCollateralDisabled(
   event: ReserveUsedAsCollateralDisabledEvent
 ): void {
-  let entity = new LendingPoolReserveUsedAsCollateralDisabled(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleReserveUsedAsCollateralEnabled(
   event: ReserveUsedAsCollateralEnabledEvent
 ): void {
-  let entity = new LendingPoolReserveUsedAsCollateralEnabled(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
 
 export function handleSwap(event: SwapEvent): void {
-  let entity = new LendingPoolSwap(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._reserve = event.params._reserve
-  entity._user = event.params._user
-  entity._newRateMode = event.params._newRateMode
-  entity._newRate = event.params._newRate
-  entity._borrowBalanceIncrease = event.params._borrowBalanceIncrease
-  entity._timestamp = event.params._timestamp
-  entity.save()
+  handleToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    // todo
+  );
 }
