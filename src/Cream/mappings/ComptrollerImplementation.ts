b916956ed6616d3ee5f458b0a36992ef8f701136
undermethod <shaunlerner@gmail.com>
2021-07-07 19:59:56 -0600

    initial CREAM and Fulcrum-bZx integration

---
+++ src/Cream/mappings/ComptrollerImplementation.ts
@@ -0,0 +1,77 @@
+import { log } from "@graphprotocol/graph-ts";
+import {
+  ComptrollerImplementation,
+  CompSpeedUpdated as CompSpeedUpdatedEvent,
+} from "../../../generated/ComptrollerImplementation/ComptrollerImplementation";
+import { CreamToken } from "../../../generated/CreamToken/CreamToken";
+import { CreamTokenData } from "../../../generated/schema";
+import { convertBINumToDesiredDecimals } from "../../utils/converters";
+
+export function handleCompSpeedUpdated(event: CompSpeedUpdatedEvent): void {
+  let comptrollerContract = ComptrollerImplementation.bind(event.address);
+  let tokenContract = CreamToken.bind(event.params.cToken);
+
+  let entity = CreamTokenData.load(event.transaction.hash.toHex());
+  if (!entity) entity = new CreamTokenData(event.transaction.hash.toHex());
+
+  let underlyingAssetDecimals: i32;
+  let underlyingAsset = tokenContract.try_underlying();
+  if (underlyingAsset.reverted) log.error("underlying() reverted", []);
+  else {
+    let underlyingAssetContract = CreamToken.bind(underlyingAsset.value);
+    if (!underlyingAssetContract) log.error("No underlyingAsset at {}", [ underlyingAsset.value.toHex() ]);
+    else {
+      let tried_underlyingAssetDecimals = underlyingAssetContract.try_decimals();
+      if (tried_underlyingAssetDecimals.reverted) log.error("decimals() reverted", []);
+      else underlyingAssetDecimals = tried_underlyingAssetDecimals.value;
+    }
+  }
+
+  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.address = event.params.cToken;
+  entity.symbol = tokenContract.symbol();
+
+  if (event.params.newSpeed) {
+    entity.compSpeeds = convertBINumToDesiredDecimals(event.params.newSpeed, 18);
+  } else {
+    let tried_compSpeeds = comptrollerContract.try_compSpeeds(event.params.cToken);
+    if (tried_compSpeeds.reverted) log.error("compSpeeds() reverted", []);
+    else entity.compSpeeds = convertBINumToDesiredDecimals(tried_compSpeeds.value, 18);
+  }
+  
+  let tried_borrowIndex = tokenContract.try_borrowIndex();
+  if (tried_borrowIndex.reverted) log.error("borrowIndex() reverted", []);
+  else entity.borrowIndex = tried_borrowIndex.value;
+
+  let tried_borrowRatePerBlock = tokenContract.try_borrowRatePerBlock();
+  if (tried_borrowRatePerBlock.reverted) log.error("borrowRatePerBlock() reverted", []);
+  else entity.borrowRatePerBlock = convertBINumToDesiredDecimals(tried_borrowRatePerBlock.value, 18);
+
+  let tried_supplyRatePerBlock = tokenContract.try_supplyRatePerBlock();
+  if (tried_supplyRatePerBlock.reverted) log.error("supplyRatePerBlock() reverted", []);
+  else entity.supplyRatePerBlock = convertBINumToDesiredDecimals(tried_supplyRatePerBlock.value, 18);
+  
+  let tried_exchangeRateStored = tokenContract.try_exchangeRateStored();
+  if (tried_exchangeRateStored.reverted) log.error("exchangeRateStored() reverted", []);
+  else entity.exchangeRateStored = convertBINumToDesiredDecimals(tried_exchangeRateStored.value, 10 + underlyingAssetDecimals);
+
+  let tried_getCash = tokenContract.try_getCash();
+  if (tried_getCash.reverted) log.error("getCash() reverted", []);
+  else entity.totalCash = convertBINumToDesiredDecimals(tried_getCash.value, underlyingAssetDecimals);
+
+  let tried_totalBorrows = tokenContract.try_totalBorrows();
+  if (tried_totalBorrows.reverted) log.error("totalBorrows() reverted", []);
+  else entity.totalBorrows = convertBINumToDesiredDecimals(tried_totalBorrows.value, underlyingAssetDecimals);
+
+  let tried_totalSupply = tokenContract.try_totalSupply();
+  if (tried_totalSupply.reverted) log.error("totalSupply() reverted", []);
+  else entity.totalSupply = convertBINumToDesiredDecimals(tried_totalSupply.value, underlyingAssetDecimals);
+
+  let tried_totalReserves = tokenContract.try_totalReserves();
+  if (tried_totalReserves.reverted) log.error("totalReserves() reverted", []);
+  else entity.totalReserves = convertBINumToDesiredDecimals(tried_totalReserves.value, underlyingAssetDecimals);
+  
+  entity.save();
+}
---
+++ src/Fulcrum/mappings/FulcrumToken.ts
@@ -0,0 +1,96 @@
+import { log, Address, Bytes, BigInt } from "@graphprotocol/graph-ts";
+import {
+  FulcrumToken,
+  Burn as BurnEvent,
+  FlashBorrow as FlashBorrowEvent,
+  Mint as MintEvent,
+} from "../../../generated/FulcrumToken/FulcrumToken";
+import { FulcrumTokenData } from "../../../generated/schema";
+import { convertBINumToDesiredDecimals } from "../../utils/converters";
+
+function handleFulcrumToken(
+  transactionHash: Bytes,
+  blockNumber: BigInt,
+  blockTimestamp: BigInt,
+  address: Address,
+  tokenPrice: BigInt,
+): void {
+  let tokenContract = FulcrumToken.bind(address);
+
+  let entity = FulcrumTokenData.load(transactionHash.toHex());
+  if (entity == null) {
+    entity = new FulcrumTokenData(transactionHash.toHex());
+  }
+
+  entity.transactionHash = transactionHash;
+  entity.blockNumber = blockNumber;
+  entity.blockTimestamp = blockTimestamp;
+  entity.address = address;
+
+  let tried_symbol = tokenContract.try_symbol();
+  if (tried_symbol.reverted) log.error("symbol() reverted", []);
+  else entity.symbol = tried_symbol.value;
+
+  log.debug("Saving Fulcrum Token {} at address {} in block {} with txHash {}", [
+    tried_symbol.value,
+    address.toHex(),
+    blockNumber.toString(),
+    transactionHash.toHex(),
+  ]);
+
+  let tried_supplyInterestRate = tokenContract.try_supplyInterestRate();
+  if (tried_supplyInterestRate.reverted) log.error("supplyInterestRate() reverted", []);
+  else entity.supplyInterestRate = convertBINumToDesiredDecimals(tried_supplyInterestRate.value, 20);
+
+  let tried_borrowInterestRate = tokenContract.try_borrowInterestRate();
+  if (tried_borrowInterestRate.reverted) log.error("borrowInterestRate() reverted", []);
+  else entity.borrowInterestRate = convertBINumToDesiredDecimals(tried_borrowInterestRate.value, 20);
+
+  if (tokenPrice) {
+    entity.tokenPrice = convertBINumToDesiredDecimals(tokenPrice, 18);
+  } else {
+    let tried_tokenPrice = tokenContract.try_tokenPrice();
+    if (tried_tokenPrice.reverted) log.error("tokenPrice() reverted", []);
+    else entity.tokenPrice = convertBINumToDesiredDecimals(tried_tokenPrice.value, 18);
+  }
+
+  let tried_marketLiquidity = tokenContract.try_marketLiquidity();
+  if (tried_marketLiquidity.reverted) log.error("marketLiquidity() reverted", []);
+  else entity.marketLiquidity = convertBINumToDesiredDecimals(tried_marketLiquidity.value, 18);
+
+  let tried_totalSupply = tokenContract.try_totalSupply();
+  if (tried_totalSupply.reverted) log.error("totalSupply() reverted", []);
+  else entity.totalSupply = convertBINumToDesiredDecimals(tried_totalSupply.value, 18);
+
+  entity.save();
+}
+
+export function handleBurn(event: BurnEvent): void {
+  handleFulcrumToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    event.params.price,
+  );
+}
+
+export function handleFlashBorrow(event: FlashBorrowEvent): void {
+  handleFulcrumToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    null, // tokenPrice
+  );
+}
+
+export function handleMint(event: MintEvent): void {
+  handleFulcrumToken(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    event.params.price,
+  );
+}
