623bb092326071a198274a89d4db3ac02c0f3c5b
undermethod <shaunlerner@gmail.com>
2021-07-13 19:01:06 -0600

    initial AaveV1

--- adapters/AaveV1/src/mapping.AToken.ts
+++ adapters/AaveV1/src/mapping.AToken.ts
@@ -1,17 +1,12 @@
 import { log, Address, Bytes, BigInt } from "@graphprotocol/graph-ts";
 import {
-  Approval as ApprovalEvent,
-  BalanceTransfer as BalanceTransferEvent,
+  AToken,
   BurnOnLiquidation as BurnOnLiquidationEvent,
-  InterestRedirectionAllowanceChanged as InterestRedirectionAllowanceChangedEvent,
-  InterestStreamRedirected as InterestStreamRedirectedEvent,
   MintOnDeposit as MintOnDepositEvent,
-  Redeem as RedeemEvent,
-  RedirectedBalanceUpdated as RedirectedBalanceUpdatedEvent,
-  Transfer as TransferEvent
-} from "./AToken";
-import { FromTokenToPool, AaveV1Token } from "../generated/schema";
-// import { AaveTokenV1 as UnderlyingAsset } from "./AaveTokenV1";
+} from "../generated/AToken/AToken";
+import { AaveV1TokenData } from "../generated/schema";
+import { LendingPoolAddressesProvider } from "../generated/AToken/LendingPoolAddressesProvider";
+import { LendingPoolDataProvider } from "../generated/AToken/LendingPoolDataProvider";
 import { convertBINumToDesiredDecimals } from "./converters";
 
 function handleToken(
@@ -19,29 +14,67 @@ function handleToken(
   blockNumber: BigInt,
   blockTimestamp: BigInt,
   address: Address,
-  // todo
 ): void {
-  // todo
-}
+  let POOL_PROVIDER_ADDRESS: Address = Address.fromString("0x24a42fD28C976A61Df5D00D0599C34c4f90748c8");
 
-export function handleApproval(event: ApprovalEvent): void {
-  handleToken(
-    event.transaction.hash,
-    event.block.number,
-    event.block.timestamp,
-    event.address,
-    // todo
-  );
-}
+  let tokenContract = AToken.bind(address);
 
-export function handleBalanceTransfer(event: BalanceTransferEvent): void {
-  handleToken(
-    event.transaction.hash,
-    event.block.number,
-    event.block.timestamp,
-    event.address,
-    // todo
-  );
+  let entity = AaveV1TokenData.load(transactionHash.toHex());
+  if (!entity) entity = new AaveV1TokenData(transactionHash.toHex());
+
+  entity.transactionHash = transactionHash;
+  entity.blockNumber = blockNumber;
+  entity.blockTimestamp = blockTimestamp;
+  entity.address = address;
+  entity.symbol = tokenContract.symbol();
+
+  log.debug("Saving AaveV1 Token {} at address {} in block {} with txHash {}", [
+    entity.symbol,
+    address.toHex(),
+    blockNumber.toString(),
+    transactionHash.toHex(),
+  ]);
+
+  let underlyingAssetAddr = tokenContract.underlyingAssetAddress();
+  entity.decimals = tokenContract.decimals();
+
+  let poolProviderContract = LendingPoolAddressesProvider.bind(POOL_PROVIDER_ADDRESS);
+  let dataProviderContract = LendingPoolDataProvider.bind(poolProviderContract.getLendingPoolDataProvider());
+  
+  let tried_getReserveConfigurationData = dataProviderContract.try_getReserveConfigurationData(underlyingAssetAddr);
+  if (tried_getReserveConfigurationData.reverted) log.error("dataProvider at {} call getReserveConfigurationData({}) reverted", [ dataProviderContract._address.toHex(), underlyingAssetAddr.toHex() ]);
+  else {
+    let reserveConfData = tried_getReserveConfigurationData.value;
+    entity.ltv = convertBINumToDesiredDecimals(reserveConfData.value0, 4);
+    entity.liquidationThreshold = convertBINumToDesiredDecimals(reserveConfData.value1, 4);
+    entity.liquidationBonus = convertBINumToDesiredDecimals(reserveConfData.value2, 4);
+    entity.interestRateStrategyAddress = reserveConfData.value3;
+    entity.usageAsCollateralEnabled = reserveConfData.value4;
+    entity.borrowingEnabled = reserveConfData.value5;
+    entity.stableBorrowRateEnabled = reserveConfData.value6;
+    entity.isActive = reserveConfData.value7;
+  }
+
+  let tried_getReserveData = dataProviderContract.try_getReserveData(underlyingAssetAddr);
+  if (tried_getReserveData.reverted) log.error("dataProvider at {} call getReserveConfigurationData({}) reverted", [ dataProviderContract._address.toHex(), underlyingAssetAddr.toHex() ]);
+  else {
+    let reserveData = tried_getReserveData.value;
+    entity.totalLiquidity = convertBINumToDesiredDecimals(reserveData.value0, entity.decimals);
+    entity.availableLiquidity = convertBINumToDesiredDecimals(reserveData.value1, entity.decimals);
+    entity.totalBorrowsStable = convertBINumToDesiredDecimals(reserveData.value2, 27);
+    entity.totalBorrowsVariable = convertBINumToDesiredDecimals(reserveData.value3, 27);
+    entity.liquidityRate = convertBINumToDesiredDecimals(reserveData.value4, 27);
+    entity.variableBorrowRate = convertBINumToDesiredDecimals(reserveData.value5, 27);
+    entity.stableBorrowRate = convertBINumToDesiredDecimals(reserveData.value6, 27);
+    entity.averageStableBorrowRate = convertBINumToDesiredDecimals(reserveData.value7, 27);
+    entity.utilizationRate = convertBINumToDesiredDecimals(reserveData.value8, 27);
+    entity.liquidityIndex = reserveData.value9;
+    entity.variableBorrowIndex = reserveData.value10;
+    entity.aTokenAddress = reserveData.value11;
+    entity.lastUpdateTimestamp = reserveData.value12;
+  }
+
+  entity.save();
 }
 
 export function handleBurnOnLiquidation(event: BurnOnLiquidationEvent): void {
@@ -50,31 +83,6 @@ export function handleBurnOnLiquidation(event: BurnOnLiquidationEvent): void {
     event.block.number,
     event.block.timestamp,
     event.address,
-    // todo
-  );
-}
-
-export function handleInterestRedirectionAllowanceChanged(
-  event: InterestRedirectionAllowanceChangedEvent
-): void {
-  handleToken(
-    event.transaction.hash,
-    event.block.number,
-    event.block.timestamp,
-    event.address,
-    // todo
-  );
-}
-
-export function handleInterestStreamRedirected(
-  event: InterestStreamRedirectedEvent
-): void {
-  handleToken(
-    event.transaction.hash,
-    event.block.number,
-    event.block.timestamp,
-    event.address,
-    // todo
   );
 }
 
@@ -84,38 +92,5 @@ export function handleMintOnDeposit(event: MintOnDepositEvent): void {
     event.block.number,
     event.block.timestamp,
     event.address,
-    // todo
-  );
-}
-
-export function handleRedeem(event: RedeemEvent): void {
-  handleToken(
-    event.transaction.hash,
-    event.block.number,
-    event.block.timestamp,
-    event.address,
-    // todo
-  );
-}
-
-export function handleRedirectedBalanceUpdated(
-  event: RedirectedBalanceUpdatedEvent
-): void {
-  handleToken(
-    event.transaction.hash,
-    event.block.number,
-    event.block.timestamp,
-    event.address,
-    // todo
-  );
-}
-
-export function handleTransfer(event: TransferEvent): void {
-  handleToken(
-    event.transaction.hash,
-    event.block.number,
-    event.block.timestamp,
-    event.address,
-    // todo
   );
 }
