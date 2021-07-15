458f6b94bef11ff406aaf26f55dac902d387d402
Dhruvin Parikh <29928916+dhruvinparikh@users.noreply.github.com>
2021-07-15 11:55:01 -0400

    Opty 181 (#6) (#15)
    * added Harvest protocol
    
    * combine pool data and vault data Harvest into 1 Harvest subgraph
    
    * added .env folder, changed subgraph
    
    * changed from String to HexString
    
    * added null check value
    
    * changed check condition
    
    * Changed schema and added update data logic
    
    * removed duplicate harvest abi
    
    * added new logic
    
    * fixed requested changes
    
    * added requested changes
    
    * Update harvest/src/utils/convertors.ts
    
    Co-authored-by: Deepanshu Gupta <312.gupta@gmail.com>
    
    * added minor changes
    
    * fixed requested changes
    
    Co-authored-by: Deepanshu Gupta <312.gupta@gmail.com>
    
    Co-authored-by: Leo Tuan Dinh <54964744+leodinh@users.noreply.github.com>
    Co-authored-by: Deepanshu Gupta <312.gupta@gmail.com>

---
+++ harvest/src/utils/convertors.ts
@@ -0,0 +1,66 @@
+import { BigInt, BigDecimal, Address, Bytes } from "@graphprotocol/graph-ts"
+
+//  function to get the 0 number in BigDecimal format
+export function zeroBD(): BigDecimal {
+  return BigDecimal.fromString("0")
+}
+
+//  function to get the 0 number in BigInt format
+export function zeroBI(): BigInt {
+  return BigInt.fromI32(0)
+}
+
+// @ts-ignore
+//  function to convert the defined decimals into Graph's BigDecimal format
+export function exponentToBigDecimal(decimals: i32): BigDecimal {
+  let bd = BigDecimal.fromString("1")
+  let bd10 = BigDecimal.fromString("10")
+  for (let i = 0; i < +decimals; i++) {
+    bd = bd.times(bd10)
+  }
+  return bd
+}
+
+// @ts-ignore
+//  function to convert the BigInt number into real time values post dividing with the defined decimals
+export function convertBINumToDesiredDecimals(
+  value: BigInt,
+  decimals: i32
+): BigDecimal {
+  return value.toBigDecimal().div(exponentToBigDecimal(decimals))
+}
+
+//  function to convert the string to lower case
+export function convertToLowerCase(str: string): string {
+  // create a result variable
+  let result = ""
+
+  for (let i = 0; i < str.length; i++) {
+    // get the code of the current character
+    let code = str.charCodeAt(i)
+
+    // check if it's within the range of capital letters
+    if (code > 64 && code < 91) {
+      // if so, add a new character to the result string
+      // of the character from our code, plus 32
+      result += String.fromCharCode(code + 32)
+    } else {
+      // otherwise, just add the current character
+      result += str.charAt(i)
+    }
+  }
+
+  // return the result
+  return result
+}
+
+export function convertStringToAddress(addr: string): Address {
+  return Address.fromString(addr.toString())
+}
+
+//  function to get the 0 address
+export function zeroAddress(): Bytes {
+  return <Bytes>(
+    Bytes.fromHexString("0x0000000000000000000000000000000000000000")
+  )
+}
