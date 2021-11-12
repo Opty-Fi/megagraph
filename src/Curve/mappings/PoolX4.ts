e4f2388c874d776b129c2aa7fbf3d7cca72901ac
Uwe Voelker <uwe.voelker@xing.com>
2021-11-12 21:27:59 +0100

    add missing events

--- src/Curve/mappings/PoolX4.ts
+++ src/Curve/mappings/PoolX4.ts
@@ -2,6 +2,7 @@ import {
   AddLiquidity as AddLiquidityEvent,
   RemoveLiquidity as RemoveLiquidityEvent,
   RemoveLiquidityImbalance as RemoveLiquidityImbalanceEvent,
+  TokenExchange as TokenExchangeEvent,
   TokenExchangeUnderlying as TokenExchangeUnderlyingEvent,
 } from "../../../generated/CurvePoolX4yDAI+yUSDC+yUSDT+yTUSD/CurvePoolX4";
 import { Curve_N_COINS_CURVE4POOL } from "../../utils/constants";
@@ -30,6 +31,17 @@ export function handleRemoveLiquidity(event: RemoveLiquidityEvent): void {
 }
 
 export function handleRemoveLiquidityImbalance(event: RemoveLiquidityImbalanceEvent): void {
+  handlePoolEntity(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address, // vault
+    Curve_N_COINS_CURVE4POOL,
+    "Curve4Pool"
+  );
+}
+
+export function handleTokenExchange(event: TokenExchangeEvent): void {
   handlePoolEntity(
     event.transaction.hash,
     event.block.number,
