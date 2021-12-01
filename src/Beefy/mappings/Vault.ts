c56f83a43dae6448a385dbf239430eba726b09ee
Uwe Voelker <uwe.voelker@xing.com>
2021-12-01 23:29:45 +0100

    Beefy on Polygon (WIP)

---
+++ src/Beefy/mappings/Vault.ts
@@ -0,0 +1,13 @@
+import {
+  Transfer as TransferEvent,
+} from "../../../generated/BeefyVaultbifi-maxi/BeefyVault";
+import { handleEntity } from "./handlers";
+
+export function handleTransfer(event: TransferEvent): void {
+  handleEntity(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+  );
+}
