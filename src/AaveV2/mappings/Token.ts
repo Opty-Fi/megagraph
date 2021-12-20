1da36924ba5c9588f4da70e9742c6599bcc5c8e6
Kuririn <kuririn1@gmail.com>
2021-12-20 16:32:11 +0100

    small fixes

--- src/AaveV2/mappings/Token.ts
+++ src/AaveV2/mappings/Token.ts
@@ -2,7 +2,7 @@ import { log, Address, Bytes, BigInt } from "@graphprotocol/graph-ts";
 import { AaveV2Token, Burn as BurnEvent, Mint as MintEvent } from "../../../generated/AaveV2TokenaDAI/AaveV2Token";
 import { AaveV2TokenData, AaveV2Reserve } from "../../../generated/schema";
 import { AaveV2LendingPoolAddressesProvider } from "../../../generated/AaveV2TokenaDAI/AaveV2LendingPoolAddressesProvider";
-import { AaveV2IncentivesController } from "../../../generated/AaveV2TokenaDAI/AaveV2IncentivesController"; // ADD ABI FOR IT!!!
+import { AaveV2IncentivesController } from "../../../generated/AaveV2TokenaDAI/AaveV2IncentivesController";
 import { AaveV2AaveProtocolDataProvider } from "../../../generated/AaveV2TokenaDAI/AaveV2AaveProtocolDataProvider";
 import { convertBINumToDesiredDecimals } from "../../utils/converters";
 import { AaveV2_POOL_PROVIDER_ADDRESS, AaveV2_DATA_PROVIDER_INDEX } from "../../utils/constants";
@@ -12,7 +12,7 @@ function handleAaveV2Token(
   blockNumber: BigInt,
   blockTimestamp: BigInt,
   address: Address,
-  value: BigInt
+  value: BigInt,
 ): void {
   let tokenContract = AaveV2Token.bind(address);
 
@@ -40,21 +40,21 @@ function handleAaveV2Token(
 
   let poolProviderContract = AaveV2LendingPoolAddressesProvider.bind(AaveV2_POOL_PROVIDER_ADDRESS);
   let tried_getDataProvider = poolProviderContract.try_getAddress(AaveV2_DATA_PROVIDER_INDEX);
-  if (tried_getDataProvider.reverted)
+  if (tried_getDataProvider.reverted) {
     log.error("poolProvider at {} call getDataProvider({}) reverted", [
       poolProviderContract._address.toHex(),
       AaveV2_DATA_PROVIDER_INDEX.toHex(),
     ]);
-  else {
+  } else {
     let dataProviderContract = AaveV2AaveProtocolDataProvider.bind(tried_getDataProvider.value);
 
     let tried_getReserveConfigurationData = dataProviderContract.try_getReserveConfigurationData(underlyingAssetAddr);
-    if (tried_getReserveConfigurationData.reverted)
+    if (tried_getReserveConfigurationData.reverted) {
       log.error("dataProvider at {} call getReserveConfigurationData({}) reverted", [
         dataProviderContract._address.toHex(),
         underlyingAssetAddr.toHex(),
       ]);
-    else {
+    } else {
       let reserveConfData = tried_getReserveConfigurationData.value;
       entity.decimals = reserveConfData.value0.toI32();
       entity.ltv = convertBINumToDesiredDecimals(reserveConfData.value1, 4);
@@ -69,12 +69,12 @@ function handleAaveV2Token(
     }
 
     let tried_getReserveData = dataProviderContract.try_getReserveData(underlyingAssetAddr);
-    if (tried_getReserveData.reverted)
+    if (tried_getReserveData.reverted) {
       log.error("dataProvider at {} call getReserveConfigurationData({}) reverted", [
         dataProviderContract._address.toHex(),
         underlyingAssetAddr.toHex(),
       ]);
-    else {
+    } else {
       let reserveData = tried_getReserveData.value;
       entity.availableLiquidity = convertBINumToDesiredDecimals(reserveData.value0, entity.decimals);
       entity.totalStableDebt = convertBINumToDesiredDecimals(reserveData.value1, entity.decimals);
@@ -91,19 +91,23 @@ function handleAaveV2Token(
 
   //get aEmissionsPerSecond
   let tried_getIncentivesController = tokenContract.try_getIncentivesController();
-  if (tried_getIncentivesController.reverted) log.error("tokenContract at {} call getIncentivesController() reverted", [ tokenContract._address.toHex() ]); 
-  else {
+  if (tried_getIncentivesController.reverted) {
+    log.error("tokenContract at {} call getIncentivesController() reverted", [tokenContract._address.toHex()]);
+  } else {
     let incentivesControllerAddress = tried_getIncentivesController.value;
     let incentivesControllerContract = AaveV2IncentivesController.bind(incentivesControllerAddress);
     let tried_assets = incentivesControllerContract.try_assets(address);
 
-    if (tried_assets.reverted) log.error("AaveIncentivesController at {} call assets({}) reverted", [ incentivesControllerContract._address.toHex(), underlyingAssetAddr.toHex() ]);
-    else {
+    if (tried_assets.reverted) {
+      log.error("AaveIncentivesController at {} call assets({}) reverted", [
+        incentivesControllerContract._address.toHex(),
+        underlyingAssetAddr.toHex(),
+      ]);
+    } else {
       let assets = tried_assets.value;
       entity.aEmissionPerSecond = assets.value0;
     }
   }
-
 
   //get totalLiquidity per token
   let reserveId = address.toHexString();
@@ -114,7 +118,7 @@ function handleAaveV2Token(
     reserve.address = address;
     reserve.symbol = entity.symbol;
     reserve.totalLiquidity = BigInt.fromI32(0);
-  }  
+  }
 
   reserve.totalLiquidity = reserve.totalLiquidity.plus(value);
   entity.totalLiquidity = reserve.totalLiquidity;
@@ -124,9 +128,21 @@ function handleAaveV2Token(
 }
 
 export function handleBurn(event: BurnEvent): void {
-  handleAaveV2Token(event.transaction.hash, event.block.number, event.block.timestamp, event.address, event.params.value.neg());
+  handleAaveV2Token(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    event.params.value.neg(),
+  );
 }
 
 export function handleMint(event: MintEvent): void {
-  handleAaveV2Token(event.transaction.hash, event.block.number, event.block.timestamp, event.address, event.params.value);
+  handleAaveV2Token(
+    event.transaction.hash,
+    event.block.number,
+    event.block.timestamp,
+    event.address,
+    event.params.value,
+  );
 }
