c44fd6c4cfafacfb6dc3114b0c50a2429b2126e0
michelet <michelet@usp.br>
2022-01-15 20:35:07 -0300

    working with the supply

--- src/Mai/mappings/handlers.ts
+++ src/Mai/mappings/handlers.ts
@@ -2,7 +2,8 @@ import { Address, BigInt, Bytes, log } from "@graphprotocol/graph-ts";
 import { MaiCamTokenData } from "../../../generated/schema";
 import { MaiCamToken } from "../../../generated/MaiCamTokencamWMATIC/MaiCamToken";
 import { MaiFinanceFarmAddress, ZERO_BD, ZERO_BI } from "../../utils/constants";
-export function handleCamToken(txnHash: Bytes, blockNumber: BigInt, timestamp: BigInt, camTokenAddress: Address) {
+
+export function handleCamToken(txnHash: Bytes, blockNumber: BigInt, timestamp: BigInt, camTokenAddress: Address): void {
   let entity = MaiCamTokenData.load(txnHash.toHex());
   if (!entity) entity = new MaiCamTokenData(txnHash.toHex());
   let camContract = MaiCamToken.bind(camTokenAddress);
@@ -12,5 +13,16 @@ export function handleCamToken(txnHash: Bytes, blockNumber: BigInt, timestamp: B
   if (totalSupplyResult.reverted) {
     log.warning("totalSupply() reverted", []);
   } else {
+    entity.totalSupply = totalSupplyResult.value;
   }
+  let symbolResult = camContract.try_symbol();
+  if (symbolResult.reverted) {
+    log.warning("totalSupply() reverted", []);
+  } else {
+    entity.symbol = symbolResult.value;
+  }
+
+  entity.vault = camTokenAddress;
+
+  entity.save();
 }
