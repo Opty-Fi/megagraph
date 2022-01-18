aa50a11d1a42f33081b57a49283c847e805eb459
michelet <michelet@usp.br>
2022-01-18 17:14:33 -0300

    WIP: EmissionPerSecond issue

--- src/Mai/mappings/handlers.ts
+++ src/Mai/mappings/handlers.ts
@@ -21,20 +21,19 @@ export function handleCamToken(
   if (!entity) entity = new MaiCamTokenData(txnHash.toHex());
   entity.blockNumber = blockNumber;
   entity.blockTimestamp = timestamp;
-  let amLiquidity = ZERO_BI;
-  // get right underlying reserve contract.
-  let reserveAddress = ZERO_ADDRESS;
 
   // lending pool is the same for camToken, camAave and camWMatic
-  // the abi for the are the same, except for the
   let camContract = MaiCamToken.bind(camTokenAddress);
+
+  // get correct underlying reserve contract address.
+  let reserveAddress = ZERO_ADDRESS;
   let amTokenResult = camContract.try_usdc();
   if (amTokenResult.reverted) {
     log.warning("try_usdc() reverted", []);
     // try aave
     let camAaveContract = MaiCamAave.bind(camTokenAddress);
-    let amWethResult = camAaveContract.try_aave();
-    if (amWethResult.reverted) {
+    let amAaveResult = camAaveContract.try_aave();
+    if (amAaveResult.reverted) {
       log.warning("try_weth() reverted", []);
       // try WMatic
       let camWMaticContract = MaiCamWMatic.bind(camTokenAddress);
@@ -45,27 +44,28 @@ export function handleCamToken(
         reserveAddress = amWMaticResult.value;
       }
     } else {
-      reserveAddress = amWethResult.value;
+      reserveAddress = amAaveResult.value;
     }
   } else {
     reserveAddress = amTokenResult.value;
   }
+  // reserveAddress found
 
+  // updating entity symbol
   let symbolResult = camContract.try_symbol();
   if (symbolResult.reverted) {
     log.warning("totalSupply() reverted", []);
   } else {
     entity.symbol = symbolResult.value;
   }
-
+  // updating entity decimals
   let decimalsResult = camContract.try_decimals();
   if (decimalsResult.reverted) {
     log.warning("try_decimals() reverted", []);
   } else {
     entity.decimals = decimalsResult.value;
   }
-
-  // get emissions per second through the amToken incentives controller
+  // finding amToken info and updating amToken
   let amContractResult = camContract.try_Token();
   if (amContractResult.reverted) {
     log.error("try_Token() reverted", []);
@@ -84,27 +84,23 @@ export function handleCamToken(
     amToken.blockNumber = blockNumber;
     amToken.blockTimestamp = timestamp;
 
-    // let aTokenScaledTotalSupplyResult = amATokenContract.try_scaledTotalSupply();
-
     let aTokenDecimalsResult = amATokenContract.try_decimals();
     if (aTokenDecimalsResult.reverted) {
       log.warning("aToken.try_decimals() reverted", []);
     } else {
       amToken.decimals = aTokenDecimalsResult.value;
     }
+    amToken.totalLiquidity = amToken.totalLiquidity.plus(value);
+    amToken.save();
 
-    // if (aTokenScaledTotalSupplyResult.reverted) {
-    //   log.warning("total_supply() reverted", []);
-    // } else {
-    //
-    // }
-
+    // get emissions per second through the amToken incentives controller
     let incentivesControllerResult = camContract.try_AaveContract();
     if (incentivesControllerResult.reverted) {
       log.error("tokenContract at {} call getAaveContract() reverted", [camContract._address.toHex()]);
     } else {
       let incentivesControllerAddress = incentivesControllerResult.value;
       let incentivesControllerContract = MaiIncentivesController.bind(incentivesControllerAddress);
+
       let assetsResult = incentivesControllerContract.try_assets(amContract);
 
       if (assetsResult.reverted) {
@@ -114,13 +110,12 @@ export function handleCamToken(
         ]);
       } else {
         let assets = assetsResult.value;
-        entity.aEmissionPerSecond = assets.value0; //emissionPerSecond
+        // https://docs.aave.com/developers/guides/apy-and-apr
+        //
+        entity.aEmissionPerSecond = assets.value0; //emissionPerSecond, expressed in WEI
       }
     }
 
-    amToken.totalLiquidity = amToken.totalLiquidity.plus(value);
-    //amToken.totalLiquidity = amLiquidity;
-    amToken.save();
     entity.aTvl = convertBINumToDesiredDecimals(amToken.totalLiquidity, entity.decimals);
     // get camToken info through it's lending controller
     let lendingControllerResult = camContract.try_LENDING_POOL();
@@ -142,19 +137,21 @@ export function handleCamToken(
         let liquidityIndex = camTokenConfiguration.liquidityIndex;
         let variableBorrowIndex = camTokenConfiguration.variableBorrowIndex;
         entity.aTokenAddress = aTokenAddress;
-        entity.liquidityRate = convertBINumToDesiredDecimals(liquidityRate, entity.decimals);
-        entity.stableBorrowRate = convertBINumToDesiredDecimals(stableBorrowRate, entity.decimals);
-        entity.variableBorrowRate = convertBINumToDesiredDecimals(variableBorrowRate, entity.decimals);
-        entity.liquidityIndex = convertBINumToDesiredDecimals(liquidityIndex, entity.decimals);
-        entity.variableBorrowRate = convertBINumToDesiredDecimals(variableBorrowIndex, entity.decimals);
+        // https://docs.aave.com/developers/guides/apy-and-apr
+        // Rates
+        entity.liquidityRate = convertBINumToDesiredDecimals(liquidityRate, 27);
+        entity.stableBorrowRate = convertBINumToDesiredDecimals(stableBorrowRate, 27);
+        entity.variableBorrowRate = convertBINumToDesiredDecimals(variableBorrowRate, 27);
+        entity.liquidityIndex = convertBINumToDesiredDecimals(liquidityIndex, 27);
+        entity.variableBorrowRate = convertBINumToDesiredDecimals(variableBorrowIndex, 27);
       }
     }
   }
-  let totalSupplyResult = camContract.try_totalSupply();
-  if (totalSupplyResult.reverted) {
+  let camTokenTotalLiquidityResult = camContract.try_totalSupply();
+  if (camTokenTotalLiquidityResult.reverted) {
     log.warning("totalSupply() reverted", []);
   } else {
-    entity.totalLiquidity = totalSupplyResult.value;
+    entity.totalLiquidity = camTokenTotalLiquidityResult.value;
   }
   entity.save();
 }
